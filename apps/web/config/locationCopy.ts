2a975eef2ab329177b22e63aa7dc0146eb8622fd
Ernest Juchheim <juchheim@gmail.com>
2025-11-13 12:51:27 -0600

    Add missing locationCopy config file for build

---
+++ apps/web/config/locationCopy.ts
@@ -0,0 +1,8 @@
+export const locationCopy = {
+  emptyQueryError: "Please enter a location.",
+  geocodeGenericError: "We couldn't find that location. Try a full address, city & state, or ZIP code.",
+  enterLocationOrProvideCoords: "Enter a location or provide coordinates in Advanced options.",
+  provideCountyFipsOrLocation: "Enter a location or provide a county FIPS in Advanced options.",
+  locationBlocked: "Location is blocked. Type an address instead.",
+  browserNoGeolocation: "Your browser does not support geolocation. Please enter a location instead.",
+};
